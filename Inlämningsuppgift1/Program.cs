1efb650d1806bbf0f0569609600359f755699d96
Jesper Persson <mrjesperpersson@hotmail.se>
2020-11-18 16:05:53 +0100

    Add project files.

---
+++ Inlämningsuppgift1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inlämningsuppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
