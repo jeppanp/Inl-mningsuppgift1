12829420172e110392c69edf7bba27066304e704
Jesper Persson <mrjesperpersson@hotmail.se>
2020-11-18 16:27:58 +0100

    Created an "Operator"-Method.

--- Inlämningsuppgift1/Program.cs
+++ Inlämningsuppgift1/Program.cs
@@ -8,6 +8,40 @@ namespace Inlämningsuppgift1
         {
             Console.Title = "Fucked up calculator";
 
+            Console.WriteLine("Enter your first operator: ");
+
+
+        }
+
+
+        // User choice for an operator 
+        static char Operator()
+        {
+            char operantInChar = '0';
+            bool myBool = true;
+            do
+            {
+                string operatorInString = Console.ReadLine();
+                if (operatorInString == "*" || operatorInString == "/" || operatorInString == "-" || operatorInString == "+" || operatorInString == "%")
+                {
+                    try
+                    {
+                        operantInChar = Convert.ToChar(operatorInString);
+
+                        myBool = false;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Please try to enter an operant /, *, +, -, %");
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You can only choose between \"+, - , * , / , %\" ");
+                }
+            } while (myBool);
+            return operantInChar;
         }
     }
 }
